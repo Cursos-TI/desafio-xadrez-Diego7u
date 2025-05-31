42bec912362c1b7a1368217ba7f0785ab15b0595
Diego7u <109025623+Diego7u@users.noreply.github.com>
2025-05-31 20:05:04 +0000

    adicione a peca cavalo eo movimento em L do cavalo

--- xadrez.c
+++ xadrez.c
@@ -2,6 +2,8 @@
 #define MOVIMENTO_TORRE 5
 #define MOVIMENTO_BISPO 5
 #define MOVIMENTO_RAINHA 8
+#define MOVIMENTO_CAVALO 1
+// Definição dos movimentos das peças de xadrez
 
 int main() {
     int i;
@@ -27,6 +29,17 @@ int main() {
         printf("Esquerda\n");
         i++;
     } while (i <= MOVIMENTO_RAINHA);
+    // Movimento do Cavalo (for loop)
+    printf("\nMovimento do Cavalo:\n");
+    for (i = 1; i <= MOVIMENTO_CAVALO; i++) {
+        printf("Esquerda\n");
+        int j;
+        for (j = 0; j <= 1; j++)
+        {
+            printf("Baixo\n");
+        }
+        
+    }
 
     return 0;
 }
